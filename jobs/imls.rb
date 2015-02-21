3949a3d77381f29d9e448685e1cc4c6da4fb5fc6
Mark Silverberg <mark.silverberg@socrata.com>
2015-02-21 16:29:01 -0500

    added count by state top/bottom widget

--- jobs/imls.rb
+++ jobs/imls.rb
@@ -28,19 +28,18 @@ SCHEDULER.every '5m', first_in: 0 do |job|
   # # Send event to dashboard
   # send_event('count_by_status', { items: count_by_status.values })
 
+  # museums per state - top 5 / low 5
+  # breakdown by type
+  # number with EIN; percent with EIN
 
-  # #### TOTAL TICKETS ####
+  # #### TOTAL MUSEUMS ####
   total_museums_response = soda_client.get(dataset_resource_id, {
     "$select" => "count(*)"
   })
   total_museums = total_museums_response.first["count"].to_i
   send_event('total_museums', { current:  total_museums})
 
-  # museums per state - top 5 / low 5
-  # breakdown by type
-  # number with EIN; percent with EIN
-
-  #### TOTAL WITH PHOTO ####
+  #### TOTAL NONPROFIT ####
   total_nonprofit_response = soda_client.get(dataset_resource_id, {
     "$where" => "ein is not null",
     "$select" => "count(*)"
@@ -48,20 +47,22 @@ SCHEDULER.every '5m', first_in: 0 do |job|
   total_nonprofit = total_nonprofit_response.first["count"]
   send_event('total_nonprofit', { current:  total_nonprofit})
 
-  # #### PERCENT WITH PHOTO ####
+  # #### PERCENT NONPROFIT ####
   percent_nonprofit = ((total_nonprofit.to_f/total_museums.to_f)*100).to_i
   send_event('percent_nonprofit', { value:  percent_nonprofit})
 
-  # #### COUNT BY ISSUE TYPE ####
-  # count_by_issue_type_response = soda_client.get(dataset_resource_id, {
-  #   "$group" => "issue_type",
-  #   "$select" => "issue_type, COUNT(issue_type) AS n"
-  # })
-  # count_by_issue_type = {}
-  # count_by_issue_type_response.each do |item|
-  #   count_by_issue_type[item.issue_type] = {:label => item.issue_type, :value => item.n}
-  # end
-  # # Send event to dashboard
-  # send_event('count_by_issue_type', { items: count_by_issue_type.values.sort_by{|x| x[:value].to_i}.reverse })
 
+  #### COUNT BY ISSUE TYPE ####
+  count_by_state_response = soda_client.get(dataset_resource_id, {
+    "$group" => "state",
+    "$select" => "state, COUNT(*) AS n"
+  })
+  count_by_state = {}
+  count_by_state_response.each do |item|
+    count_by_state[item.state] = {:label => item.state, :value => item.n}
+  end
+  count_by_state_in_order = count_by_state.values.sort_by{|x| x[:value].to_i}.reverse
+  count_by_state_to_send = count_by_state_in_order[0,7]+[{:label => "..."}]+count_by_state_in_order[-7,7]
+  # Send event to dashboard
+  send_event('count_by_state', { items: count_by_state_to_send })
 end
